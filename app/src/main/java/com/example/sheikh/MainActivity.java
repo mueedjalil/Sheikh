817a6e404361944f595457a6605083588049930c
mueedjalil <mueedjalil3655@gmail.com>
2020-10-22 10:22:38 +0500

    Commit Initial

--- app/src/main/java/com/example/sheikh/MainActivity.java
+++ app/src/main/java/com/example/sheikh/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sheikh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hello
+        Button btn;
     }
 }
